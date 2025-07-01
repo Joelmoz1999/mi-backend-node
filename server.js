8471fe4b8242b7e3768305bb045e1071ce0abc91
joel <joel-13-2@outlook.com>
2025-07-01 11:29:22 -0500

    h

--- server.js
+++ server.js
@@ -167,7 +167,7 @@ app.post('/generar-pdf', async (req, res) => {
     // Configuración de márgenes por línea (¡Personaliza estos valores!)
     const lineSettings = {
       firstLine: { startX: 140, endX: 240, startY: 150 }, // Ancho: 300 (440-140)
-      secondLine: { startX: 120, endX: 240, startY: 165 }, // Misma anchura, 15pt arriba
+      secondLine: { startX: 120, endX: 240, startY: 170 }, // Misma anchura, 15pt arriba
       thirdLine: { startX: 120, endX: 240, startY: 180 }  // Misma anchura, 15pt más arriba
     };
 
