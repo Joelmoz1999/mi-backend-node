6017b2b6dbe1873704accfc6fa5f5f7ae39b5945
joel <joel-13-2@outlook.com>
2025-07-01 10:23:12 -0500

    22

--- server.js
+++ server.js
@@ -166,9 +166,9 @@ app.post('/generar-pdf', async (req, res) => {
 
     // Configuración de márgenes por línea (¡Personaliza estos valores!)
     const lineSettings = {
-      firstLine: { startX: 140, endX: 420, startY: 150 }, // Ancho: 300 (440-140)
-      secondLine: { startX: 130, endX: 425, startY: 140 }, // Misma anchura, 15pt arriba
-      thirdLine: { startX: 125, endX: 428, startY: 120 }   // Misma anchura, 15pt más arriba
+      firstLine: { startX: 140, endX: 200, startY: 150 }, // Ancho: 300 (440-140)
+      secondLine: { startX: 130, endX: 210, startY: 140 }, // Misma anchura, 15pt arriba
+      thirdLine: { startX: 125, endX: 205, startY: 120 }   // Misma anchura, 15pt más arriba
     };
 
     const font = await pdfDoc.embedFont(StandardFonts.Helvetica);
