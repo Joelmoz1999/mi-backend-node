fe52f5ac61b6103bfbc47848053751eabe1d7427
joel <joel-13-2@outlook.com>
2025-07-01 11:41:46 -0500

    se queda ested

--- server.js
+++ server.js
@@ -166,9 +166,9 @@ app.post('/generar-pdf', async (req, res) => {
 
     // Configuración de márgenes por línea (¡Personaliza estos valores!)
     const lineSettings = {
-      firstLine: { startX: 140, endX: 240, startY: 150 }, // Ancho: 300 (440-140)
-      secondLine: { startX: 120, endX: 240, startY: 170 }, // Misma anchura, 15pt arriba
-      thirdLine: { startX: 120, endX: 240, startY: 180 }  // Misma anchura, 15pt más arriba
+      firstLine: { startX: 140, endX: 340, startY: 150 }, // Ancho: 300 (440-140)
+      secondLine: { startX: 120, endX: 240, startY: 130 }, // Misma anchura, 15pt arriba
+      thirdLine: { startX: 120, endX: 240, startY: 120 }  // Misma anchura, 15pt más arriba
     };
 
     const font = await pdfDoc.embedFont(StandardFonts.Helvetica);
