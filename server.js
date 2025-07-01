97a03e319b603ed482b76e118f2190a2a05abadf
joel <joel-13-2@outlook.com>
2025-07-01 12:08:13 -0500

    se queda estedddhhyhjhysdfdvdf

--- server.js
+++ server.js
@@ -164,9 +164,9 @@ app.post('/generar-pdf', async (req, res) => {
 
 
 
-   / Configuración de márgenes por línea (¡Personaliza estos valores!)
+
     const lineSettings = {
-      firstLine: { startX: 140, endX: 340, startY: 150 }, // Ancho: 300 (440-140)
+      firstLine: { startX: 140, endX: 330, startY: 150 }, // Ancho: 300 (440-140)
       secondLine: { startX: 120, endX: 240, startY: 130 }, // Misma anchura, 15pt arriba
       thirdLine: { startX: 120, endX: 240, startY: 120 }  // Misma anchura, 15pt más arriba
     };
