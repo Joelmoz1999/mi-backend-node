b8eee5b5fce3e407faa7b2384d7d2718b03ea375
jooel123 <ajmunozsa@uide.edu.ec>
2025-11-28 15:50:17 -0500

    razon8

--- server.js
+++ server.js
@@ -167,7 +167,7 @@ app.post('/generar-pdf', async (req, res) => {
 
     const lineSettings = {
       firstLine: { startX: 140, endX: 340, startY: 150 }, // Ancho: 300 (440-140)
-      secondLine: { startX: 180, endX: 240, startY: 130 }, // Misma anchura, 15pt arriba
+      secondLine: { startX: 200, endX: 240, startY: 130 }, // Misma anchura, 15pt arriba
       thirdLine: { startX: 90, endX: 240, startY: 120 }  // Misma anchura, 15pt más arriba
     };
 
